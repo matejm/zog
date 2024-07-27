ff6b05e823f7d82fa3ecabd1d034a8204a0c4914
Matej Marinko <matej@zerodays.dev>
2024-07-27 20:52:24 +0200

    feat: add optional type

---
+++ zog/optional.go
@@ -0,0 +1,61 @@
+package zog
+
+import (
+	"reflect"
+)
+
+type optionalSchema[T any] struct {
+	// child schema
+	schema SchemaDefinition[T]
+}
+
+// Optional creates a new optional schema.
+// There are two possible usages of optional schema:
+//  1. outer optional schema:
+//     zog.Optional(zog.String())
+//  2. chained optional schema:
+//     zog.String().Optional()
+//
+// Both value and value pointers are valid.
+func Optional[T any](schema SchemaDefinition[T]) *optionalSchema[T] {
+	return &optionalSchema[T]{
+		schema: schema,
+	}
+}
+
+func (s *boolSchema) Optional() *optionalSchema[bool] {
+	return Optional(s)
+}
+
+func (s *intSchema) Optional() *optionalSchema[int] {
+	return Optional(s)
+}
+
+func (s *stringSchema) Optional() *optionalSchema[string] {
+	return Optional(s)
+}
+
+func (s *objectSchema[T]) Optional() *optionalSchema[T] {
+	return Optional(s)
+}
+
+func (s *arraySchema[T]) Optional() *optionalSchema[[]T] {
+	return Optional(s)
+}
+
+func (s *optionalSchema[T]) Parse(data any) (*T, error) {
+	if data == nil {
+		return nil, nil
+	}
+
+	// if data is a pointer, dereference it
+	if reflect.TypeOf(data).Kind() == reflect.Ptr {
+		data = reflect.ValueOf(data).Elem().Interface()
+	}
+
+	parsed, err := s.schema.Parse(data)
+	if err != nil {
+		return nil, err
+	}
+	return &parsed, nil
+}
---
+++ zog/optional_test.go
@@ -0,0 +1,67 @@
+package zog_test
+
+import (
+	"testing"
+
+	"github.com/matejm/zog/zog"
+	"github.com/stretchr/testify/suite"
+)
+
+type OptionalTestSuite struct {
+	suite.Suite
+}
+
+func (t *OptionalTestSuite) TestInvalidType() {
+	schema := zog.Optional((zog.String()))
+
+	_, err := schema.Parse(1)
+	t.Error(err)
+
+	number := 1
+	_, err = schema.Parse(&number)
+	t.Error(err)
+}
+
+func (t *OptionalTestSuite) TestValid() {
+	schema := zog.Optional(zog.String())
+
+	// both string and *string are valid
+	str := "a"
+
+	v, err := schema.Parse(str)
+	t.Equal("a", *v)
+	t.Nil(err)
+
+	v, err = schema.Parse(&str)
+	t.Equal("a", *v)
+	t.Nil(err)
+
+	// nil is valid
+	v, err = schema.Parse(nil)
+	t.Nil(v)
+	t.Nil(err)
+}
+
+func (t *OptionalTestSuite) TestChained() {
+	schema := zog.String().Optional()
+	v, err := schema.Parse("a")
+	t.Equal("a", *v)
+	t.Nil(err)
+
+	v, err = schema.Parse(nil)
+	t.Nil(v)
+	t.Nil(err)
+
+	schema2 := zog.Bool().Optional()
+	v2, err := schema2.Parse(true)
+	t.Equal(true, *v2)
+	t.Nil(err)
+
+	v2, err = schema2.Parse(nil)
+	t.Nil(v2)
+	t.Nil(err)
+}
+
+func TestOptionalSuite(t *testing.T) {
+	suite.Run(t, new(OptionalTestSuite))
+}
